fafe4ed01e73831e6fcf09e347a27bb0bcd15a59
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-04 13:23:10 +0200

    Corrected Augmentations.

--- src/mura/tfds_from_disc.py
+++ src/mura/tfds_from_disc.py
@@ -4,17 +4,24 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 import tf2lib as tl
-import tensorflow_addons as tfa
+from matplotlib import pyplot as plt
+from skimage.io import imread
 
 
 def make_dataset(img_paths, batch_size, load_size, crop_size, training, drop_remainder=True, shuffle=True, repeat=1,
                  labels=None, special_normalisation=None):
+    """
+    Returns a preprocesed batched dataset. If train=True then augmentations are applied.
+    """
     if training:
         @tf.function
         def _map_fn(img, label=None):  # preprocessing
             img = tf.cast(img, tf.float32)
-            img = tfa.image.equalize(img)
             img = tf.image.random_flip_left_right(img)
+            img = tf.image.random_contrast(img, 0.3, 0.8)
+            img = tf.image.random_brightness(img, 0.3)
+            gamma = tf.random.uniform(minval=0.9, maxval=1.1, shape=[1, ])
+            img = tf.image.adjust_gamma(img, gamma=gamma[0])
             img = tf.image.resize_with_pad(img, load_size, load_size, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
             img = tf.image.random_crop(img, [crop_size, crop_size, tf.shape(img)[-1]])
             if not special_normalisation:
@@ -161,8 +168,8 @@ def get_mura_ds_by_body_part_split_class(body_parts, tfds_path, batch_size, crop
                                                       special_normalisation=special_normalisation)
 
     A_B_dataset_valid, _ = make_zip_dataset(A_valid, B_valid, batch_size, load_size,
-                                    crop_size, training=False, repeat=True,
-                                    special_normalisation=special_normalisation)
+                                            crop_size, training=False, repeat=True,
+                                            special_normalisation=special_normalisation)
 
     A_B_dataset_test, _ = make_zip_dataset(A_test, B_test, batch_size, load_size,
                                            crop_size, training=False, repeat=True,
@@ -199,3 +206,44 @@ def get_mura_ds_by_body_part(body_parts, tfds_path, batch_size, crop_size, load_
 
 """get_mura_ds_by_body_part_split_class('XR_WRIST', "/Users/dimitrymindlin/tensorflow_datasets", 32, 256, 256,
                                      special_normalisation=None)"""
+
+"""def tf_augmentations(image, label):
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_contrast(image, 0.3, 0.8)
+    image = tf.image.random_brightness(image, 0.3)
+    gamma = tf.random.uniform(minval=0.9, maxval=1.1, shape=[1, ])
+    image = tf.image.adjust_gamma(image, gamma=gamma[0])
+    return image, label"""
+
+
+def show_tf_augmentations():
+    root = '/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/'
+    image = imread(root + 'MURA-v1.1_transformed/train/XR_WRIST/patient00136/study1_positive/image3.png')
+
+    def visualize(original, augmented):
+        fig = plt.figure()
+        plt.subplot(1, 2, 1)
+        plt.title('Original image')
+        plt.imshow(original, cmap='gray')
+
+        plt.subplot(1, 2, 2)
+        plt.title('Augmented image')
+        plt.imshow(augmented, cmap='gray')
+        plt.show()
+
+    if len(image.shape) < 3:
+        image = tf.expand_dims(image, axis=-1)
+    if image.shape[-1] != 3:
+        image = tf.image.grayscale_to_rgb(image)
+    image = tf.image.resize_with_pad(image, 512, 512)
+    image = tf.math.divide(image, 255.)
+    # image = tf.expand_dims(image, axis=0)
+    image_aug = tf.image.random_flip_left_right(image)
+    visualize(image, image_aug)
+    image_aug = tf.image.random_contrast(image, 0.3, 0.8)
+    visualize(image, image_aug)
+    image_aug = tf.image.random_brightness(image, 0.3)
+    visualize(image, image_aug)
+    gamma = tf.random.uniform(minval=0.9, maxval=1.1, shape=[1, ])
+    image_aug = tf.image.adjust_gamma(image, gamma=gamma[0])
+    visualize(image, image_aug)
