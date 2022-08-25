ee3950064fd228572808dd49be4d72bad96161b5
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-25 22:51:58 +0200

    Corrected test set.

--- src/mura/tfds_from_disc.py
+++ src/mura/tfds_from_disc.py
@@ -237,7 +237,7 @@ def get_mura_test_ds_by_body_part_split_class(body_parts, tfds_path, batch_size,
 
     A_dataset_test = make_dataset(A_test, batch_size, load_size, crop_size, training=False, drop_remainder=True,
                                   shuffle=False, repeat=1, special_normalisation=special_normalisation)
-    B_dataset_test = make_dataset(B_train, batch_size, load_size, crop_size, training=False, drop_remainder=True,
+    B_dataset_test = make_dataset(B_test, batch_size, load_size, crop_size, training=False, drop_remainder=True,
                                   shuffle=False, repeat=1, special_normalisation=special_normalisation)
 
     return A_dataset, B_dataset, A_dataset_test, B_dataset_test
